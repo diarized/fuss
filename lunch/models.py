ec8a21bbbdcc67b706e80c9b58b4b6914ae1725a
Artur Kamiński <my.home.my.castle@gmail.com>
2013-03-01 21:50:02 +0100

    Lunch - all dir

--- lunch/models.py
+++ lunch/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    vendor = models.ForeignKey(Vendor, null=False)
+    image = models.ImageField(upload_to=settings.MEDIA_ROOT, null=True, blank=True)
+
+
+class Event(models.Model):
+    owner = User
+    date_created = models.DateTimeField()
+    vendor = models.ForeignKey(Vendor)
+    close_date = models.DateTimeField()
+
+
+class OrderedMeal(models.Model):
+    requester = User
+    event = models.ForeignKey(Event)
+    meal = models.ForeignKey(Meal)
+
+
